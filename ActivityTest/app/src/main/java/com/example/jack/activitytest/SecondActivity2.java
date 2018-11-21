5bf6bf03ced5d38c0a944358af8ab025c11a1d4d
alwaysquietll <alwaysquietll@gmail.com>
2018-11-21 20:25:43 +0800

    second commit

--- ActivityTest/app/src/main/java/com/example/jack/activitytest/SecondActivity2.java
+++ ActivityTest/app/src/main/java/com/example/jack/activitytest/SecondActivity2.java
@@ -1,8 +1,10 @@
 package com.example.jack.activitytest;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +17,7 @@ public class SecondActivity2 extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Log.d("tag","onCreate");
         //初始化button
         Button mButton = findViewById(R.id.btn);
         //设置点击事件监听
@@ -30,5 +33,41 @@ public class SecondActivity2 extends AppCompatActivity{
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("TAG", "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("TAG", "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TAG", "onDestroy: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TAG", "onPause: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("TAG", "onResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("TAG", "onRestart: ");
     }
 }
